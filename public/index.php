0665960eff0b3fc71db5f11962ce5a6eaa261794
Grishin <grishinma2009@yandex.ru>
2022-04-09 19:39:34 +0300

    added some routes

--- public/index.php
+++ public/index.php
@@ -13,4 +13,20 @@ $app->get('/', function ($request, $response) {
     return $response;
 });
 
+$app->get('/users', function ($request, $response) {
+    $response->getBody()->write('GET /users');
+    return $response;
+});
+
+$app->post('/users', function ($request, $response) {
+    return $response->withStatus(302);
+});
+
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    $response->getBody()->write("Course id is {$id}");
+    return $response;
+});
+
+
 $app->run();
