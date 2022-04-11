625666d798bb98f45c769a3ed4c948d43c99d0ee
Grishin <grishinma2009@yandex.ru>
2022-04-12 01:16:19 +0300

    add some routes & templates

--- public/index.php
+++ public/index.php
@@ -4,18 +4,39 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
+
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+    // $this в Slim это контейнер зависимостей
+    return $this->get('renderer')->render($response, '/users/show.phtml', $params);
+});
 
 $app->get('/', function ($request, $response) {
     $response->getBody()->write("welcome to Slim!");
     return $response;
 });
 
-$app->get('/users', function ($request, $response) {
-    $response->getBody()->write('GET /users');
-    return $response;
+$app->get('/users', function ($request, $response) use ($users) {
+    $term = $request->getQueryParams();
+    $filteredUsers = array_filter($users, fn($user) => str_contains($user, $term['term']));
+    $params = [
+        'users' => $filteredUsers,
+        'term' => $term['term']
+    ];
+    return $this->get('renderer')->render($response, '/users/index.phtml', $params);
 });
 
 $app->post('/users', function ($request, $response) {
@@ -23,6 +44,7 @@ $app->post('/users', function ($request, $response) {
 });
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
+    print_r($args);
     $id = $args['id'];
     $response->getBody()->write("Course id is {$id}");
     return $response;
