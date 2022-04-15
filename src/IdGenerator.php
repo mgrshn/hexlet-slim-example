136d964ef8b7b020ad4d6882db7e4f38f80fb124
Grishin <grishinma2009@yandex.ru>
2022-04-15 16:05:53 +0300

    made idGenerator() staticly

--- src/IdGenerator.php
+++ src/IdGenerator.php
@@ -4,7 +4,7 @@ namespace App;
 
 class IdGenerator
 {
-    public function generateId(): int 
+    public static function generateId(): int 
     {
         $users = json_decode(file_get_contents(__DIR__ . '/../storage/users.json'), true);
         return isset($users) ? count($users) + 1 : 1;
